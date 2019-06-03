0bbc0eec5c0b2aa292a2687257881d53bc29c298
Ian Theis <iandbtheis@gmail.com>
2019-06-03 18:31:03 -0500

    port config

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 var exphbs = require("express-handlebars")
 var db = require('./models')
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/magicScraper";
 
